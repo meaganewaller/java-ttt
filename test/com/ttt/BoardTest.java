e026ed813833d9e39a33ac3474a6caecfbcdfa28
Meagan Waller <meaganewaller@gmail.com>
2013-12-11 10:20:03 -0500

    Refactors tests

--- test/com/ttt/BoardTest.java
+++ test/com/ttt/BoardTest.java
@@ -2,25 +2,29 @@ package com.ttt;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BoardTest {
+	Board board;
+
+	@Before
+	public void setUp() {
+		board = new Board(3);
+	}
 	
 	@Test
 	public void hasASize() {
-		Board board = new Board(3);
 		assertEquals(3, board.getSize());
 	}
 	
 	@Test
 	public void hasSpaces() {
-		Board board = new Board(3);
 		assertEquals("---------", board.getSpaces());
 	}
 	
 	@Test
 	public void setsXMove() {
-		Board board = new Board(3);
 		board.setMove('X', 5);
 		assertEquals("----X----", board.getSpaces());
 	}
