0a1889f4ec24117425028c91ccc2085c387c5eed
Meagan Waller <meaganewaller@gmail.com>
2013-12-12 14:40:44 -0500

    Adds minimax for unbeatable ai

--- src/com/ttt/UnbeatableAI.java
+++ src/com/ttt/UnbeatableAI.java
@@ -1,19 +1,80 @@
 package com.ttt;
 
-public class UnbeatableAI implements AI {
+import java.util.ArrayList;
 
+public class UnbeatableAI implements AI {
+	private BoardRules boardRules;
 	private char marker;
+	private int MAX_SCORE = 100;
+	private int MIN_SCORE = -100;
+	private int TIED_SCORE = 0;
+
+
 	public UnbeatableAI(char marker) {
 		this.marker = marker;
 	}
 	
 	public int findMove(Board board) {
-		return 0;
+		char opponentMarker = getOpponentMarker(marker);
+		int bestScore = MIN_SCORE;
+		int bestSpace = 0;
+		
+		ArrayList<Integer> emptySpaces = board.getEmptySpaces();
+		for(Integer emptySpace : emptySpaces) {
+			board.setMove(marker, emptySpace);
+			int score = miniMax(board, opponentMarker, 0, -1, false, MIN_SCORE, MAX_SCORE);
+
+			board.undoMove(emptySpace);
+			if(score > bestScore) {
+				bestScore = score;
+				bestSpace = emptySpace;
+			}
+		}
+		return bestSpace;
 	}
 	
 	public int findValue(Board board) {
-		return 100;
+		return miniMax(board, getOpponentMarker(marker), 0, -1, false, MIN_SCORE, MAX_SCORE);
 	}
+	
+	private char getOpponentMarker(char marker) {
+		if(marker == 'X') return 'O';
+		else return 'X';
+	}
+
+	private int miniMax(Board board, char marker, int depth, int color, boolean max, int alpha, int beta) {
+		char opponentMarker = getOpponentMarker(marker);
+		boardRules = new BoardRules(board);
+		if(boardRules.isOver() || depth == 7) return getBoardScore(board, depth, marker) * color;
+		else if(max) {
+			ArrayList<Integer> emptySpaces = board.getEmptySpaces();
+			for(Integer emptySpace : emptySpaces) {
+				board.setMove(marker, emptySpace);
+				alpha = Math.max(alpha, miniMax(board, opponentMarker, depth + 1, -color, !max, alpha, beta));
+				board.undoMove(emptySpace);
+				if(alpha >= beta) break;
+			}
+			return alpha;
+		} else {
+			ArrayList<Integer> emptySpaces = board.getEmptySpaces();
+			for(Integer emptySpace : emptySpaces) {
+				board.setMove(marker, emptySpace);
+				beta = Math.min(beta, miniMax(board, opponentMarker, depth + 1, -color, !max, alpha, beta));
+				board.undoMove(emptySpace);
+				if(alpha >= beta) break;
+			}
+			return beta;
+		}
+	}
+
+	private int getBoardScore(Board board, int depth, char marker) {
+		boardRules = new BoardRules(board);
+		if (boardRules.winner() == marker) return MAX_SCORE + depth;
+		else if (boardRules.tie()) return TIED_SCORE;
+		else return MIN_SCORE + depth;
+	}
+
+	
 	
 
 }
--- test/com/ttt/UnbeatableAITest.java
+++ test/com/ttt/UnbeatableAITest.java
@@ -21,5 +21,54 @@ public class UnbeatableAITest {
 		board.setMoves('O', moves);
 		assertEquals(100, ai.findValue(board));
 	}
+	
+	@Test
+	public void returnsNegative100ForLosing() {
+		int[] moves = {1, 2, 3};
+		board.setMoves('X', moves);
+		assertEquals(-100, ai.findValue(board));
+	}
+	
+	@Test
+	public void returns0ForTie() {
+		int[] xMoves = {1, 3, 5, 8};
+		int[] oMoves = {2, 4, 6, 7, 9};
+		board.setMoves('O', oMoves);
+		board.setMoves('X', xMoves);
+		assertEquals(0, ai.findValue(board));
+	}
+	
+	@Test
+	public void caculatesLossAfterMove() {
+		int[] xMoves = {1, 3, 7, 8};
+		int[] oMoves = {2, 4, 5};
+		board.setMoves('X', xMoves);
+		board.setMoves('O', oMoves);
+		assertEquals(-99, ai.findValue(board));
+	}
+	
+	@Test
+	public void returnsWinningMove() {
+		int[] xMoves = {1, 3, 7, 9};
+		int[] oMoves = {4, 5};
+		board.setMoves('X', xMoves);
+		board.setMoves('O', oMoves);
+		assertEquals(6, ai.findMove(board));
+	}
+	
+	@Test
+	public void makesGoodMove() {
+		int[] xMoves = {1, 5};
+		int[] oMoves = {9};
+		board.setMoves('X', xMoves);
+		board.setMoves('O', oMoves);
+		assertEquals(3, ai.findMove(board));
+	}
+	
+	@Test
+	public void makesValidMove() {
+		board.setMove('X', 1);
+		assertEquals(5, ai.findMove(board));
+	}
 
 }
