1b69dee95a8bb72bdb9e557cc323ad6cb4573549
Meagan Waller <meaganewaller@gmail.com>
2013-12-12 13:48:33 -0500

    Refactors test

--- test/com/ttt/ComputerPlayerTest.java
+++ test/com/ttt/ComputerPlayerTest.java
@@ -2,25 +2,30 @@ package com.ttt;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ComputerPlayerTest {
+	ComputerPlayer cpu;
+	
+	@Before
+	public void setUp() {
+		cpu = new ComputerPlayer('O');
+		
+	}
 
 	@Test
 	public void computerNotHuman() {
-		ComputerPlayer cpu = new ComputerPlayer('O');
 		assertFalse(cpu.isHuman());
 	}
 	
 	@Test
 	public void computerHasMarker() {
-		ComputerPlayer cpu = new ComputerPlayer('O');
 		assertEquals('O', cpu.getMarker());
 	}
 	
 	@Test
 	public void makesMove() {
-		ComputerPlayer cpu = new ComputerPlayer('O');
 		Board board = new Board(3);
 		board = cpu.move(board, 5);
 		assertEquals("O--------", board.getSpaces());
