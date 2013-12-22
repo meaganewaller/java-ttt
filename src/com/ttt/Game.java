2fe224defcbcbf473cb16ed1de6af6cb662f829d
Meagan Waller <meaganewaller@gmail.com>
2013-12-22 17:44:26 -0500

    Gets rid of unused methods

--- src/com/ttt/Game.java
+++ src/com/ttt/Game.java
@@ -1,40 +1,47 @@
 package com.ttt;
-
-import java.util.Hashtable;
-
 public class Game {
 	private Board board;
 	private Player firstPlayer, secondPlayer;
 	private UserInterface ui;
-	private BoardRules boardState;
+	private BoardRules boardRules;
 
 	public Game(UserInterface ui) {
 		this.ui = ui;
 	}
 	
-	public Game(UserInterface ui, Hashtable<String, String> config) {
-		this.ui = ui;
-		board = new Board(Integer.parseInt(config.get("boardSize")));
-		board.setSpaces(config.get("board"));
-		boardState = new BoardRules(board);
-		firstPlayer = getPlayer(config.get("firstPlayer"), 'X');
-		secondPlayer = getPlayer(config.get("secondPlayer"), 'O');
+	public void playGame() {
+		startGame();
+		while(!boardRules.isOver()) makePlayerMoves();
+		endGame();
 	}
 	
-	public Board getBoard() {
-		return board;
+	public void startGame() {
+		ui.welcomeMessage();
+		askSettings();
+		ui.displayBoard(board);
 	}
 	
-	public Player getFirstPlayer() {
-		return firstPlayer;
+	public void makePlayerMoves() {
+		if(makePlayerMove(firstPlayer) && !boardRules.isOver()) {
+			ui.displayBoard(board);
+			makePlayerMove(secondPlayer);
+		}
+		if(!boardRules.isOver()) ui.displayBoard(board);
 	}
 	
-	public Player getSecondPlayer() {
-		return secondPlayer;
+	public boolean makePlayerMove(Player player) {
+		if(player.isHuman()) {
+			board = player.move(board, ui.askPlayerMove());
+		} else {
+			board = player.move(board,  0);
+		}
+		return true;
 	}
 	
-	public UserInterface getUI() {
-		return ui;
+	public void askSettings() {
+		askFirstPlayer();
+		askSecondPlayer();
+		askBoardSize();
 	}
 	
 	public void askFirstPlayer() {
@@ -47,29 +54,17 @@ public class Game {
 		secondPlayer = getPlayer(second, 'O');
 	}
 	
+	public void askBoardSize() {
+		int boardSize=  ui.askBoardSize();
+		board = new Board(boardSize);
+		boardRules = new BoardRules(board);
+	}
+	
 	public Player getPlayer(String input, char marker) {
 		Player player;
 		if(input.equals("h")) player = new HumanPlayer(marker);
 		else player = new ComputerPlayer(marker);
 		return player;
-	}
-	
-	public void askBoardSize() {
-		int boardSize=  ui.askBoardSize();
-		board = new Board(boardSize);
-		boardState = new BoardRules(board);
-	}
-	
-	public void startGame() {
-		ui.welcomeMessage();
-		askSettings();
-		ui.displayBoard(board);
-	}
-	
-	public void askSettings() {
-		askFirstPlayer();
-		askSecondPlayer();
-		askBoardSize();
 	}
 	
 	public void endGame() {
@@ -82,62 +77,28 @@ public class Game {
 	}
 
 	private String result() {
-		if(boardState.tie()) return "tie";
-		else return String.valueOf(boardState.winner());
+		if(boardRules.tie()) return "tie";
+		else return String.valueOf(boardRules.winner());
 	}
 	
-	public void playGame() {
-		startGame();
-		while(!boardState.isOver()) makePlayerMoves();
-		endGame();
-	}
-	
-	public void makePlayerMoves() {
-		if(makePlayerMove(firstPlayer) && !boardState.isOver()) {
-			ui.displayBoard(board);
-			makePlayerMove(secondPlayer);
-		}
-		if(!boardState.isOver()) ui.displayBoard(board);
-	}
-	
-	public boolean makePlayerMove(Player player) {
-		if(player.isHuman()) {
-			board = player.move(board, ui.askPlayerMove());
-		} else {
-			board = player.move(board,  0);
-		}
-		return true;
+	public Board getBoard() {
+		return board;
 	}
 	
 	public BoardRules getBoardRules() {
-		return boardState;
+		return boardRules;
 	}
 	
-	private void makeMove(Player player, String move) {
-		if(player.isHuman()) board = player.move(board, Integer.parseInt(move));
-		else board = player.move(board, 0);
+	public Player getFirstPlayer() {
+		return firstPlayer;
 	}
 	
-	public static Board playGame(UserInterface ui, Hashtable<String, String> config, String move) {
-		Game game = new Game(ui, config);
-		Player player = currentPlayer(game);
-		if(game.board.isEmpty() && player.isHuman()) {
-			game.board.setMove(player.getMarker(), move);
-		} else if(game.getBoardRules().isOver()) {
-			game.ui.displayResult(game.result());
-		} else {
-			game.makeMove(player, move);
-			if(game.getBoardRules().isOver()) {
-				game.ui.displayResult(game.result());
-			}
-		}
-		return game.getBoard();
+	public Player getSecondPlayer() {
+		return secondPlayer;
 	}
 	
-	private static Player currentPlayer(Game game) {
-		char marker = game.getBoardRules().currentPlayer(game.board.getSpaces());
-		if(marker == 'X') return game.firstPlayer;
-		else return game.secondPlayer;
+	public UserInterface getUI() {
+		return ui;
 	}
 
 }
